7fde3749deba55860d56f890ea29c9945f0b04e7
Asaf-Zanjiri <60044819+Asaf-Zanjiri@users.noreply.github.com>
2021-10-26 10:32:21 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import socket
 import threading
+from hashlib import md5
 
-
-IP="0.0.0.0"
+IP = "0.0.0.0"
 PORT = 13370
 PASSWORD = "99df698e726c1a51c7e3a1b9dc468102"
 BUFFER_SIZE = 2048
@@ -10,10 +10,11 @@ g_range_list = []
 client_list = []
 unchecked_ranges = []
 
+
 def main():
     current_id = 0
 
-# Initiates server socket
+    # Initiates server socket
     server_socket = socket.socket()
     server_socket.bind((IP, PORT))
     server_socket.listen()
@@ -24,36 +25,41 @@ def main():
         # Safely send the ranges to the client.
         try:
             message = cracker.recv(BUFFER_SIZE).decode()
-        except:
+        except Exception as e:
+            print(e)
             cracker.close()
             continue
 
         if message == "Howdy":
             try:
                 cracker.send(str(current_id).encode())
-            except:
+            except Exception as e:
+                print(e)
                 cracker.close()
         else:
             cracker.close()
             continue
 
-    threading.Thread(target=handle_client, args=(cracker_address[0], current_id + PORT,)).start()
-    current_id += 1
-    cracker.close()
+        threading.Thread(target=handle_client, args=(cracker_address[0], current_id + PORT,)).start()
+        current_id += 1
+        cracker.close()
+
 
 def init_ranges():
     global g_range_list
 
     start = "aaaaaa"
     for i in range(26):
-        for j in range(1,26):
-            g_range_list.append([chr(ord(start[0]) + i) + chr(ord(start[1]) + j-1) + start[2:], chr(ord(start[0]) + i) + chr(ord(start[1]) + j) + start[2:]])
-        g_range_list.append([g_range_list[-1][-1], chr(ord(start[0])+i)+"zzzzz"])
+        for j in range(1, 26):
+            g_range_list.append([chr(ord(start[0]) + i) + chr(ord(start[1]) + j - 1) + start[2:],
+                                 chr(ord(start[0]) + i) + chr(ord(start[1]) + j) + start[2:]])
+        g_range_list.append([g_range_list[-1][-1], chr(ord(start[0]) + i) + "zzzzz"])
 
-    
+
 def get_range():
     global g_range_list
     return g_range_list.pop(0)
+
 
 def finish(passwd_hash: str, password: str):
     """
@@ -69,9 +75,11 @@ def finish(passwd_hash: str, password: str):
         # Safely send the ranges to the client.
         try:
             client_socket.send(msg.encode())
-        except:
+        except Exception as e:
+            print(e)
             client_list.remove(client_socket)  # Not sure if we need this line.
     print('Done relying finish message to crackers...')
+
 
 def handle_client(ip: str, port: int):
     """
@@ -83,37 +91,34 @@ def handle_client(ip: str, port: int):
     client_socket = socket.socket()
     client_socket.connect((ip, port))
     client_list.append(client_socket)
- 
+
     # Communicate with the client until the client disconnects or until the password was found.
     while True:
         brute_range = get_range()
         msg = '{start},{stop},{md5}'.format(start=brute_range[0], stop=brute_range[1], md5=PASSWORD)
-        client_socket.send(msg.encode())
+        # Safely send the ranges to the client.
+        try:
+            client_socket.send(msg.encode())
+        except Exception as e:
+            print(e)
+            client_list.remove(client_socket)
+            unchecked_ranges.append(brute_range)
+            break
         response = client_socket.recv(BUFFER_SIZE)  # id,true/false,md5,password
- 
+
         # Stop loop if client disconnected, remove it from the client list, and add its unchecked range to the unchecked ranges list.
         if not response:
             client_list.remove(client_socket)
             unchecked_ranges.append(brute_range)
-            return
- 
+            break
+
         response = response.decode().split(',')
         if response[1] == 'true':
-            finish(response[2], response[3])
-            return
+            if md5(response[3].encode()).hexdigest() == PASSWORD:
+                finish(response[2], response[3])
+                break
+
 
 if __name__ == "__main__":
     init_ranges()
     main()
-
-"""
-id:8200                 security
-f:1/0                   less work/stop algorithm
-if 1        
-pass:password           check password
-else if has range       if not found in the range  
-delete range            delete the range from the list
-give another range      
-else 
-give range              
-"""
