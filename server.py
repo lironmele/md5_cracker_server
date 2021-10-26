8be0b21607808eef51c5b503cfab1a2bb11fae1a
Asaf-Zanjiri <60044819+Asaf-Zanjiri@users.noreply.github.com>
2021-10-26 09:38:38 +0300

    Add files via upload
    Fixed handle_clients()
    Added finish()

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import socket
+from socket import error as socket_error
 import threading
+from hashlib import md5
 
-
-IP="0.0.0.0"
+IP = "0.0.0.0"
 PORT = 13370
 PASSWORD = "99df698e726c1a51c7e3a1b9dc468102"
 BUFFER_SIZE = 2048
@@ -11,47 +12,63 @@ g_range_list = []
 client_list = []
 unchecked_ranges = []
 
+
 def main():
     global g_id
 
     s = socket.socket()
-    s.bind((IP,PORT))
+    s.bind((IP, PORT))
     s.listen(1)
     cracker, cracker_address = s.accept()
-    
+
     try:
         message = cracker.recv(BUFFER_SIZE).decode()
     except:
-        cracker.close()
         return
 
     if message == "Howdy":
         cracker.send(str(g_id).encode())
     else:
-        cracker.close()
         return
-    
-    threading.Thread(target=handle_client, args=(cracker_address[0],g_id + PORT,)).start()
-    g_id+=1
-    cracker.close()
 
-def init_ranges():
+    threading.Thread(target=handle_client, args=(cracker_address, g_id,)).start()
+    g_id += 1
+
+
+def init_ranges() -> list:
     global g_range_list
 
     start = "aaaaaa"
     for i in range(26):
-        for j in range(1,26):
+        for j in range(1, 26):
             g_range_list.append([start, chr(ord(start[0]) + i) + chr(ord(start[1]) + j) + start[2:]])
             start = chr(ord(start[0]) + i) + chr(ord(start[1]) + j) + start[2:]
-    g_range_list.append([g_range_list[-1][-1], "zzzzzz"])
+    return [0, 0]  # PLACEHOLDER
 
-    
+
 def get_range():
-    global g_range_list
-    return g_range_list.pop(0)
+    return [0, 0]  # PLACEHOLDER
 
-def finish(hash: str, password: str):
-    pass
+
+def finish(passwd_hash: str, password: str):
+    """
+    This function lets all of the crackers know that the job was finished and a password was found.
+    :param passwd_hash: Hash of the password found by the cracker
+    :param password: Clear text password found by the cracker.
+    :return: 
+    """
+    print('--------------------------\n* Password found! *\nPassword: {passwd}\nHash: {hash}\n--------------------------').format(passwd=password, hash=passwd_hash)
+    print('Relying finish message to crackers...')
+    msg = 'finish,{md5}'.format(md5=passwd_hash)
+    for client_socket in client_list:
+        # Safely send the ranges to the client.
+        try:
+            client_socket.send(msg.encode())
+        except socket_error as e:
+            print(e)
+            client_list.remove(client_socket)  # Not sure if we need this line.
+    print('Done relying finish message to crackers...')
+
 
 def handle_client(ip: str, port: int):
     """
@@ -63,28 +80,40 @@ def handle_client(ip: str, port: int):
     client_socket = socket.socket()
     client_socket.connect((ip, port))
     client_list.append(client_socket)
- 
+
     # Communicate with the client until the client disconnects or until the password was found.
     while True:
         brute_range = get_range()
         msg = '{start},{stop},{md5}'.format(start=brute_range[0], stop=brute_range[1], md5=PASSWORD)
-        client_socket.send(msg.encode())
+        # Safely send the ranges to the client.
+        try:
+            client_socket.send(msg.encode())
+        except socket_error as e:
+            print(e)
+            client_list.remove(client_socket)
+            unchecked_ranges.append(brute_range)
+            break
         response = client_socket.recv(BUFFER_SIZE)  # id,true/false,md5,password
- 
+
         # Stop loop if client disconnected, remove it from the client list, and add its unchecked range to the unchecked ranges list.
         if not response:
             client_list.remove(client_socket)
             unchecked_ranges.append(brute_range)
-            return
- 
+            break
+
         response = response.decode().split(',')
         if response[1] == 'true':
-            finish(response[2], response[3])
-            return
+            if md5(response[3].encode()).hexdigest() == PASSWORD:
+                finish(response[2], response[3])
+                break
+
+
+            # start,end,md5
+            # id,true/false,md5,password
+            # finish,md5
+
 
 if __name__ == "__main__":
-    init_ranges()
-    print(g_range_list)
     while True:
         main()
 
